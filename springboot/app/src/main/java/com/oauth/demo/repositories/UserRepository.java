ba4d7ad2935720f14465689cc3eb004a54252a79
azizktiti <ktitiaziz827@gmail.com>
2026-03-08 13:16:26 +0100

    Delete springboot/app directory

--- springboot/app/src/main/java/com/oauth/demo/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oauth.demo.repositories;
-
-import com.oauth.demo.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
-
